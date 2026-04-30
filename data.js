644cb3e715b13fd1a6d99868682550df627a7c48
goodon180-dot <goodon180@gmail.com>
2026-04-29 22:50:30 -0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+const drivers = {
+  "K. Larson": {
+    number: 5,
+    team: "Hendrick Motorsports",
+    manufacturer: "Chevrolet",
+    starts: 14,
+    points: 418,
+    wins: 0,
+    top5: 3,
+    top10: 8,
+    dnfs: 0,
+    recent: [
+      "P2 - Bristol",
+      "P5 - Martinsville",
+      "P1 - Talladega"
+    ]
+  },
+
+  "K. Busch": {
+    number: 8,
+    team: "RCR",
+    manufacturer: "Chevrolet",
+    starts: 14,
+    points: 402,
+    wins: 2,
+    top5: 6,
+    top10: 9,
+    dnfs: 1,
+    recent: [
+      "P3 - Bristol",
+      "P1 - Charlotte",
+      "P6 - Phoenix"
+    ]
+  }
+};
